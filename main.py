5750a880afdc941e60505459c6a79a2147e009b8
Gibran0102 <gibran0102@gmail.com>
2018-12-28 23:05:23 -0600

    added rotation vector

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from vector import Vector, multiplication_of_products
 import math
 
 def main():
-    A = Vector(2, 4)
-    B = Vector(5, 2)
+    A = Vector(10, 10, 0)
+    B = Vector(10, 10, 0)
     angle = lambda alpha: print(math.degrees(math.acos(alpha)))
     
     alpha = A * B / multiplication_of_products(vector_one = A, vector_two = B)
     angle(alpha)
 
+
+    a_rotate = A.rotate(math.radians(180))
+    b_rotate = B.rotate(math.radians(180))
+    
+    print(a_rotate, b_rotate)
 
 if __name__ == "__main__":
     main()
--- vector.py
+++ vector.py
@@ -1,6 +1,5 @@
 import math 
 
-
 class Vector(object):
     def __init__(self, x, y, z = 0):
         self.x = x
@@ -12,6 +11,16 @@ class Vector(object):
     
     def dot_product(self):
         return math.sqrt(self.x**2 + self.y**2+ self.z**2)
+
+    def rotate(self, theta):
+        cos_theta = math.cos(theta)
+        sin_theta = math.sin(theta)
+        
+        if self.z <= 0:
+            self.x2 = cos_theta * self.x - sin_theta * self.y 
+            self.y2 = sin_theta * self.x + cos_theta * self.y 
+
+        return [self.x2, self.y2]
 
 
 def multiplication_of_products(vector_one, vector_two):
