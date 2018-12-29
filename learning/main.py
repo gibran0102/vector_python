772e6d2744cfda2fbdb2e2efc00fd27dd7f4154d
Gibran0102 <gibran0102@gmail.com>
2018-12-28 21:50:15 -0600

    .

---
+++ learning/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+class Vector(object):
+    def __init__(self, x, y, z = 0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __mul__(self, vector):
+        return self.x * vector.x + self.y * vector.y + self.z * vector.z
+    
+    def dot_product(self):
+        return math.sqrt(self.x**2 + self.y**2+ self.z**2)
+
+
+def multiplication_of_products(vector_one, vector_two):
+    return vector_one.dot_product() * vector_two.dot_product()
+
+
+
+def main():
+    A = Vector(2, 4)
+    B = Vector(5, 2)
+    angle = lambda alpha: print(math.degrees(math.acos(alpha)))
+    
+    alpha = A * B / multiplication_of_products(vector_one = A, vector_two = B)
+    angle(alpha)
+
+
+if __name__ == "__main__":
+    main()
