83b355b14f1920b87a2ae05685071e3dd0f56310
Gibran0102 <gibran0102@gmail.com>
2018-12-28 22:17:50 -0600

    class vector

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
+from vector import Vector, multiplication_of_products
+
 import math
-
-class Vector(object):
-    def __init__(self, x, y, z = 0):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __mul__(self, vector):
-        return self.x * vector.x + self.y * vector.y + self.z * vector.z
-    
-    def dot_product(self):
-        return math.sqrt(self.x**2 + self.y**2+ self.z**2)
-
-
-def multiplication_of_products(vector_one, vector_two):
-    return vector_one.dot_product() * vector_two.dot_product()
-
-
 
 def main():
     A = Vector(2, 4)
